f7acca9302a259ec7aa04a1b68cbb7bf31cadd79
cooldenji <denjihere409@gmail.com>
2026-06-21 08:11:19 +0530

    Add configuration file for bot settings

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  USER_BOT_TOKEN: process.env.USER_BOT_TOKEN || "YOUR_USER_BOT_TOKEN_HERE",
+  ADMIN_BOT_TOKEN: process.env.ADMIN_BOT_TOKEN || "YOUR_ADMIN_BOT_TOKEN_HERE",
+
+  // Apna Telegram User ID yahan daalo
+  OWNER_ID: 8605943790,
+
+  ADMIN_PASSWORD: process.env.ADMIN_PASSWORD || "hero.96",
+
+  BOT_NAME: "Zed Zoee Bot",
+
+  CHANNEL_LINK: "https://t.me/TGSOTR",
+  GROUP_LINK: "https://t.me/TGSTOREX",
+  SUPPORT_LINK: "https://t.me/Tgstoreapix",
+
+  DATABASE_FILE: "./store.db",
+
+  DEFAULT_RATE: 90.0,
+  DEFAULT_MIN_DEPOSIT: 20,
+  DEFAULT_REFERRAL_REWARD: 10.0
+};
